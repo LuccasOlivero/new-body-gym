80f49663ca3565c724430d57cf0be106487dc534
lucas <lucas.chorolqui@gmail.com>
2023-09-23 22:53:51 -0300

    css

---
+++ src/componenets/Navbar.jsx
@@ -0,0 +1,29 @@
+export default function NavBar() {
+  return (
+    <nav className="flex items-center bg-black w-full h-16 justify-between ">
+      <picture className="h-full">
+        <img
+          src="/logo.png"
+          alt="logo.png"
+          className="w-31 h-full inline-block"
+        />
+      </picture>
+
+      <div className="flex items-center">
+        <ul className="flex gap-6 ">
+          <li className="text-white">Home</li>
+          <li className="text-white">About</li>
+          <li className="text-white">Blog</li>
+          <li className="text-white">Pricing</li>
+          <li className="text-white">Classes</li>
+          <li className="text-white">Contact</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center gap-10 border border-gray-300">
+        <button className="bg-slate-50">+</button>
+        <h3 className="text-white">Find a gym</h3>
+      </div>
+    </nav>
+  );
+}
