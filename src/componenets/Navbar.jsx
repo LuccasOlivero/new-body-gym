f2b8c4f9046d99bfed8a03fba2f52471ce21269c
lucas <lucas.chorolqui@gmail.com>
2023-09-25 08:59:56 -0300

    updates

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,30 +1,31 @@
 export default function NavBar() {
   return (
-    <nav className="bg-black w-screen flex justify-center">
-      <div className="flex items-center gap-x-20 h-16  justify-between ">
-        <picture className="h-full ml-10 ">
-          <img
-            src="/logo.png"
-            alt="logo.png"
-            className="w-31 h-full inline-block"
-          />
-        </picture>
+    <nav className="flex flex-row h-[5.5rem] items-center justify-between py-8 px-12 top-0 left-0 right-0 w-screen z-50 bg-black font-Montserrat">
+      <picture className="h-[5rem]">
+        <img src="/logo.png" alt="logo.png" className="w-full h-full block" />
+      </picture>
 
-        <div className="flex items-center mx-52">
-          <ul className="flex gap-6 text-base">
-            <li className="text-white">Home</li>
-            <li className="text-white">About</li>
-            <li className="text-white">Blog</li>
-            <li className="text-white">Pricing</li>
-            <li className="text-white">Classes</li>
-            <li className="text-white">Contact</li>
-          </ul>
-        </div>
+      <div className="flex items-center">
+        <ul className="flex gap-6 text-lg gap-x-11 text-[1rem]">
+          <li className="text-white">Home</li>
+          <li className="text-white">About</li>
+          <li className="text-white">Blog</li>
+          <li className="text-white">Schedule</li>
+          <li className="text-white">Pricing</li>
+          <li className="text-white">Classes</li>
+          <li className="text-white">Contact</li>
+        </ul>
+      </div>
 
-        <div className="flex items-center gap-10 border border-gray-300">
-          <button className="bg-slate-50">+</button>
-          <h3 className="text-white">Find a gym</h3>
+      <div className="relative flex items-center border-2 border-gray-600 rounded-md py-[.3rem] px-[.6rem]  cursor-pointer">
+        <div className="relative bg-red-600 w-9 h-10 rounded-md text-white text-4xl">
+          <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center ">
+            +
+          </span>
         </div>
+        <h3 className="text-white text-sm ml-[.8rem] font-semibold">
+          FIND GYM NOW
+        </h3>
       </div>
     </nav>
   );
